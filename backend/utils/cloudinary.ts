acea0a29cb6e0f50b2bad85b79f1e2dd4fa2f292
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-16 17:15:29 +0100

    feat: add Cloudinary configuration for image management

---
+++ backend/utils/cloudinary.ts
@@ -0,0 +1,7 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, // Click 'View API Keys' above to copy your cloud name
+    api_key: process.env.CLOUDINARY_API_KEY, // Click 'View API Keys' above to copy your API key
+    api_secret: process.env.CLOUDINARY_API_SECRET// Click 'View API Keys' above to copy your API secret
+});
