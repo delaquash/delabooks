27a7dc6df43bdc90fe4b7f4ce8fc3ae3889f8045
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-17 14:29:02 +0100

    feat: add cors middleware for improved cross-origin resource sharing

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from "express";
+import cors from "cors";
 import authRoute from "./Routes/authRoute";
 import bookRoute from "./Routes/bookRoute";
 import { connectDB } from "./config/db";
@@ -11,6 +12,7 @@ const app = express();
 const PORT  = process.env.PORT
 
 app.use(express.json())
+app.use(cors)
 // Routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/book", bookRoute);
