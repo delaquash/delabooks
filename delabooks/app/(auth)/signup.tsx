a1cd358e74760f3788805ecb964df090a7c928f4
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-20 18:24:30 +0100

    feat: update signup component to include username input, improve layout, and enhance password visibility toggle

--- delabooks/app/(auth)/signup.tsx
+++ delabooks/app/(auth)/signup.tsx
@@ -4,17 +4,17 @@ import { Ionicons } from '@expo/vector-icons'
 import COLORS from '@/constant/color'
 
 const Signup = () => {
-  const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState("")
-    const [loading, setLoading] = useState(false)
-    const [showPassword, setShowPassword] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <KeyboardAvoidingView
       style={{ flex: 1 }}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
-      <View style={styles.container} >
+      <View style={styles.container}>
         <View style={styles.card}>
           {/* HEADER */}
           <View style={styles.header}>
@@ -25,63 +25,83 @@ const Signup = () => {
           <View style={styles.formContainer}>
             {/* Input Container */}
             <View style={styles.inputGroup}>
-              <Text style={styles.label}> Username</Text>
-               <View style={styles.inputContainer}>
-                            <Ionicons 
-                              name="mail-outline"
-                              size={25}
-                              color= {COLORS.primary}
-                              style={styles.inputIcon}
-                            />
-                            <TextInput
-                              value={email}
-                              placeholder='Please enter your email address'
-                              onChangeText={setEmail}
-                              keyboardType='email-address'
-                              // keyboardAppearance='default'
-                              style={styles.input}
-                              autoCapitalize='none'
-                              placeholderTextColor={COLORS.placeholderText}
-                            />
-                          </View>
-                        </View>
-                        {/* Password */}
-                        <View  style={styles.inputGroup}>
-                          <Text style={styles.label}>Password</Text>
-                          <View style={styles.inputContainer}>
-                            <Ionicons 
-                              name="lock-closed-outline"
-                              size={25}
-                              style={styles.inputIcon}
-                              color={COLORS.primary}
-                            />
-                            <TextInput
-                              value={password}
-                              placeholder='Please enter your password'
-                              onChangeText={setPassword}
-                              secureTextEntry={!showPassword}
-                              style={styles.input}
-                              placeholderTextColor={COLORS.placeholderText}  
-                            />
-                            <TouchableOpacity
-                              onPress={()=>setShowPassword(!showPassword)}
-                              style={styles.eyeIcon}
-                            >
-                              <Ionicons 
-                                name={showPassword ? "eye-outline" : "eye-off-outline"}
-                                size={30}
-                                color={COLORS.primary}                  
-                              />
-                            </TouchableOpacity>
-                          </View>
-                        </View>
+              <Text style={styles.label}> Name </Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="person-outline"
+                  size={25}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  value={email}
+                  placeholder="Please enter your name"
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  // keyboardAppearance='default'
+                  style={styles.input}
+                  autoCapitalize="none"
+                  placeholderTextColor={COLORS.placeholderText}
+                />
+              </View>
+              <View style={styles.inputGroup}>
+                <Text style={styles.label}> Username</Text>
+                <View style={styles.inputContainer}>
+                  <Ionicons
+                    name="mail-outline"
+                    size={25}
+                    color={COLORS.primary}
+                    style={styles.inputIcon}
+                  />
+                  <TextInput
+                    value={email}
+                    placeholder="Please enter your email address"
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                    // keyboardAppearance='default'
+                    style={styles.input}
+                    autoCapitalize="none"
+                    placeholderTextColor={COLORS.placeholderText}
+                  />
+                </View>
+              </View>
+              {/* Password */}
+              <View style={styles.inputGroup}>
+                <Text style={styles.label}>Password</Text>
+                <View style={styles.inputContainer}>
+                  <Ionicons
+                    name="lock-closed-outline"
+                    size={25}
+                    style={styles.inputIcon}
+                    color={COLORS.primary}
+                  />
+                  <TextInput
+                    value={password}
+                    placeholder="Please enter your password"
+                    onChangeText={setPassword}
+                    secureTextEntry={!showPassword}
+                    style={styles.input}
+                    placeholderTextColor={COLORS.placeholderText}
+                  />
+                  <TouchableOpacity
+                    onPress={() => setShowPassword(!showPassword)}
+                    style={styles.eyeIcon}
+                  >
+                    <Ionicons
+                      name={showPassword ? "eye-outline" : "eye-off-outline"}
+                      size={30}
+                      color={COLORS.primary}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
             </View>
           </View>
-          </View>
-        {/* </View> */}
+        </View>
+      </View>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
 export default Signup
 
@@ -104,14 +124,48 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: COLORS.border,
   },
-  header: {},
-  title:{},
-  subTitle:{},
-  formContainer:{},
-  label:{},
-  inputGroup:{},
-  inputContainer:{},
-  input:{},
-  inputIcon:{},
-  eyeIcon:{}
+  header: {
+    marginBottom: 32,
+    alignItems: "center"
+  },
+  title:{
+    fontSize: 32,
+    fontWeight: "700",
+    fontFamily: "JetBrainsMono-Medium",
+    color: COLORS.primary,
+    marginBottom: 8,
+  },
+  subTitle:{
+    fontSize: 16,
+    color: COLORS.textSecondary,
+    textAlign: "center",
+  },
+  formContainer: { marginBottom: 16 },
+  inputGroup: { marginBottom: 25 },
+  inputContainer:{
+     flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: COLORS.inputBackground,
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: COLORS.border,
+        paddingHorizontal: 12,
+  },
+  input:{
+    flex: 1,
+    height: 48,
+    color: COLORS.textDark,
+  },
+  inputIcon:{
+    marginRight: 10
+  },
+  eyeIcon:{
+    padding: 8
+  },
+  label:{
+    fontSize: 14,
+    marginBottom: 8,
+    color: COLORS.textPrimary,
+    fontWeight: "500",
+  }
 })
