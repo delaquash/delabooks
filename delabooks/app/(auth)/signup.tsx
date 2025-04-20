201abf904c32a19e86149a437c0462e889dcaabc
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-20 16:55:14 +0100

    feat: implement keyboard avoiding view in signup component for improved usability

--- delabooks/app/(auth)/signup.tsx
+++ delabooks/app/(auth)/signup.tsx
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native'
 import React from 'react'
 
 const Signup = () => {
   return (
-    <View>
-      <Text>Signup</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.container} >
+        <View style={styles.card}>
+          {/* HEADER */}
+          <View style={}>
+
+          </View>
+
+          </View>
+        </View>
+    </KeyboardAvoidingView>
   )
 }
 
