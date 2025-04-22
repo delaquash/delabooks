2a599557e14af221cd5dc127905b62ec5f392d7b
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-22 14:00:43 +0100

    feat: update cron job to use API_URI from environment variables

--- backend/utils/cron.ts
+++ backend/utils/cron.ts
@@ -1,8 +1,19 @@
+/* This TypeScript code snippet is setting up a cron job that runs every 14 minutes. Within the cron
+job function, it makes an HTTPS GET request to an API endpoint specified in the environment variable
+`API_URI`. If the response status code is 200, it logs "API is running"; otherwise, it logs "API is
+not running" along with the response status code. Additionally, it handles errors that may occur
+during the HTTPS request. */
+import dotenv from 'dotenv';
+dotenv.config();
 import cron from "cron";
 import https from "https";
 
 const job = new cron.CronJob("*/14 * * * *", function() {
-    https.get("https://delabooks.vercel.app/api/cron", (res) => {
-        
+    https.get(process.env.API_URI!, (res) => {
+        if(res.statusCode === 200) console.log("API is running");
+        else console.log("API is not running", res.statusCode);
     })
+    .on("error", (err) => console.log("Error while checking API",err));
 })
+
+export default job
