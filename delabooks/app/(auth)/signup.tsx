1ea8ea8e966c6a2ba440df618136018e45cda197
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-22 08:02:15 +0100

    feat: enhance signup component with loading state and password visibility toggle

--- delabooks/app/(auth)/signup.tsx
+++ delabooks/app/(auth)/signup.tsx
@@ -1,24 +1,34 @@
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, ActivityIndicator, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { Ionicons } from '@expo/vector-icons'
-import COLORS from '@/constant/color'
-import { useRouter} from "expo-router";
-import { useAuthStore } from "@/store/authStore"
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "@/constant/color";
+import { useRouter } from "expo-router";
+import { useAuthStore } from "@/store/authStore";
 
 const Signup = () => {
-  const router = useRouter()
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-  const {user, isLoading, register  } = useAuthStore()
+  const { user, isLoading, register } = useAuthStore();
 
-  const handleSignUp = async() => {
-    const result = await register(username, email, password)
-    if(!result.success) Alert.alert("Error", result.message)
-  }
+  const handleSignUp = async () => {
+    const result = await register(username, email, password);
+    if (!result.success) Alert.alert("Error", result.message);
+  };
 
   return (
     <KeyboardAvoidingView
@@ -106,42 +116,27 @@ const Signup = () => {
                   </TouchableOpacity>
                 </View>
               </View>
-              
-                        <TouchableOpacity
-                          style={styles.button}
-                          // onPress={handleLogin}
-                          disabled={loading}
-                        >
-                          {loading ? (
-                                <ActivityIndicator color="#fff"/>
-                          ) : (
-                            <Text 
-                            style={styles.buttonText}
-                            > 
-                              Login 
-                            </Text>
-                          )}
-                        </TouchableOpacity>
-              
-                        {/* FOOTER */}
-                        <View 
-                          style={styles.footer}
-                        >
-                          <Text
-                            style={styles.footerText}
-                          >
-                            Already have an account? 
-                          </Text>
-                        
-                          <TouchableOpacity onPress={()=>router.back()}>
-                          <Text
-                              style={styles.link}
-                            >
-                              Sign UP
-                            </Text>
-                          </TouchableOpacity>
-                        </View>
 
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleSignUp}
+                disabled={loading}
+              >
+                {loading ? (
+                  <ActivityIndicator color="#fff" />
+                ) : (
+                  <Text style={styles.buttonText}>Login</Text>
+                )}
+              </TouchableOpacity>
+
+              {/* FOOTER */}
+              <View style={styles.footer}>
+                <Text style={styles.footerText}>Already have an account?</Text>
+
+                <TouchableOpacity onPress={() => router.back()}>
+                  <Text style={styles.link}>Sign UP</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </View>
@@ -150,14 +145,14 @@ const Signup = () => {
   );
 };
 
-export default Signup
+export default Signup;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flexGrow: 1,
     backgroundColor: COLORS.background,
     padding: 20,
-    justifyContent: "center"
+    justifyContent: "center",
   },
   card: {
     backgroundColor: COLORS.cardBackground,
@@ -173,80 +168,80 @@ const styles = StyleSheet.create({
   },
   header: {
     marginBottom: 32,
-    alignItems: "center"
+    alignItems: "center",
   },
-  title:{
+  title: {
     fontSize: 32,
     fontWeight: "700",
     fontFamily: "JetBrainsMono-Medium",
     color: COLORS.primary,
     marginBottom: 8,
   },
-  subTitle:{
+  subTitle: {
     fontSize: 16,
     color: COLORS.textSecondary,
     textAlign: "center",
   },
   formContainer: { marginBottom: 16 },
   inputGroup: { marginBottom: 25 },
-  inputContainer:{
-     flexDirection: "row",
-        alignItems: "center",
-        backgroundColor: COLORS.inputBackground,
-        borderRadius: 12,
-        borderWidth: 1,
-        borderColor: COLORS.border,
-        paddingHorizontal: 12,
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: COLORS.inputBackground,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    paddingHorizontal: 12,
   },
-  input:{
+  input: {
     flex: 1,
     height: 48,
     color: COLORS.textDark,
   },
-  inputIcon:{
-    marginRight: 10
+  inputIcon: {
+    marginRight: 10,
   },
-  eyeIcon:{
-    padding: 8
+  eyeIcon: {
+    padding: 8,
   },
-  label:{
+  label: {
     fontSize: 14,
     marginBottom: 8,
     color: COLORS.textPrimary,
     fontWeight: "500",
   },
-   button: {
-      backgroundColor: COLORS.primary,
-      borderRadius: 12,
-      height: 50,
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 16,
-      shadowColor: COLORS.black,
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.1,
-      shadowRadius: 4,
-      elevation: 2,
-    },
-    buttonText: {
-      fontSize: 16,
-      color: COLORS.white,
-      fontWeight: "600"
-    },
-    footer: {
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 36
-    },
-    footerText:{
-      color: COLORS.textSecondary,
-      marginRight: 5
-    },
-    link: {
-      color: COLORS.primary,
-      fontWeight: "600"
-    }
-})
+  button: {
+    backgroundColor: COLORS.primary,
+    borderRadius: 12,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 16,
+    shadowColor: COLORS.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: COLORS.white,
+    fontWeight: "600",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 36,
+  },
+  footerText: {
+    color: COLORS.textSecondary,
+    marginRight: 5,
+  },
+  link: {
+    color: COLORS.primary,
+    fontWeight: "600",
+  },
+});
 
 // 09136166795
