5b28fa31a6141662556ff5668adb07f3fd1ccf9a
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-24 14:23:24 +0100

    feat: enhance TabsLayout component with safe area insets and improved tab options

--- delabooks/app/(tabs)/_layout.tsx
+++ delabooks/app/(tabs)/_layout.tsx
@@ -1,11 +1,70 @@
+import COLORS from "@/constant/color";
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 
 export default function TabsLayout() {
+    const insects = useSafeAreaInsets()
     return (
-        <Tabs>
-            <Tabs.Screen name="create" />
-            <Tabs.Screen name="index" />
-            <Tabs.Screen name="profile" />
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: COLORS.primary,
+                headerTitleStyle: {
+                    fontWeight: '600',
+                    color: COLORS.textPrimary
+                },
+                headerShowVisible: false,
+                tabBarStyle: {
+                    borderTopWidth: 1,
+                    borderTopColor: COLORS.border,
+                    backgroundColor: COLORS.cardBackground, 
+                    paddingTop: 5,
+                    paddingBottom: insects.bottom,
+                    height: insects.top + 50
+                }
+            }}
+        >
+            <Tabs.Screen 
+                name="index" 
+                options={{ 
+                    title: "Home",
+                    tabBarIcon: ({ color, size }: {color: string, size: string}) => (
+                        <Ionicons
+                            name="home-outline"
+                            size={30}
+                            color={color}
+                        />  
+                    )
+                }}
+            />
+            <Tabs.Screen 
+                name="create" 
+                options={{ 
+                    title: "Create",
+                    tabBarIcon: ({ color, size }: {color: string, size: string}) => (
+                        <Ionicons
+                            name="add-circle-outline"
+                            size={30}
+                            color={color}
+                        />  
+                    )
+                }} 
+            />
+            <Tabs.Screen 
+                name="profile" 
+                options={{ 
+                    title: "Profile",
+                    tabBarIcon: ({ color, size }: {color: string, size: string}) => (
+                        <Ionicons
+                            name="person-outline"
+                            size={30}
+                            color={color}
+                        />  
+                    )
+                }}
+            />
         </Tabs>
     )
 }
