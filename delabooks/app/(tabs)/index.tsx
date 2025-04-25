8e1bcd8cd4ef40a021a73c43b88630e1dc925e72
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-25 16:27:51 +0100

    feat: implement Home component with loading and pagination state management in index.tsx

--- delabooks/app/(tabs)/index.tsx
+++ delabooks/app/(tabs)/index.tsx
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 
-const index = () => {
-  return (
+const Home = () => {
+  const [loading, setLoading] = useState(true)
+  const [refreshing, setRefreshing] = useState(false)
+  const [page, setPage] = useState(1)
+  const [hasMore, setHasMore] = useState(true)
+
+  const renderItem=({ item }) => {
     <View>
-      <Text>index</Text>
+
+    </View>
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={books}
+        renderItem={renderItem}
+        keyExtractor={(item)=> item._id}
+        contentContainerStyle={styles.contentContainer}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   )
 }
 
-export default index
+export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {},
+  contentContainer:{},
+  bookCard:{},
+  bookHeader:{},
+  userInfo:{},
+  avatar:{},
+  username:{}
+})
