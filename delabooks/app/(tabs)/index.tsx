a744abdf39075ac71256d33c5fe101c79fc3d330
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-25 16:54:28 +0100

    feat: enhance Home component with user image and username display in book cards

--- delabooks/app/(tabs)/index.tsx
+++ delabooks/app/(tabs)/index.tsx
@@ -1,17 +1,37 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
 import React, { useState } from 'react'
+
+interface  ItemUser {
+  image: string
+    user: {
+      image: string;
+      username: string;
+      user: string;
+
+  }
+}
 
 const Home = () => {
   const [loading, setLoading] = useState(true)
   const [refreshing, setRefreshing] = useState(false)
   const [page, setPage] = useState(1)
   const [hasMore, setHasMore] = useState(true)
+  const [books, setBooks] = useState([])
 
-  const renderItem=({ item }) => {
-    <View>
 
+  const renderItem=({ item }:{item: ItemUser}) => (
+    <View style={styles.bookCard}>
+      <View style={styles.bookHeader}>
+        <View style={styles.userInfo}>
+          <Image source={{ uri: item.user.image}} style={styles.avatar}/>
+          <Text style={styles.username}>{item.user.username}</Text>
+        </View>
+      </View>
+      <View style={styles.bookImageContainer}>
+        <Image style={styles.bookImage} source={{ uri:item.image }}  />
+      </View>
     </View>
-  }
+  )
 
   return (
     <View style={styles.container}>
@@ -35,5 +55,7 @@ const styles = StyleSheet.create({
   bookHeader:{},
   userInfo:{},
   avatar:{},
-  username:{}
+  username:{},
+  bookImageContainer:{},
+  bookImage:{}
 })
