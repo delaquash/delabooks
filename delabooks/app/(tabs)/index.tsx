cd0904cc9f1759ecbe1ed1f22daf947fe18ad777
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-25 17:19:05 +0100

    feat: refactor Home component to include API integration for fetching books and enhance type definitions

--- delabooks/app/(tabs)/index.tsx
+++ delabooks/app/(tabs)/index.tsx
@@ -1,7 +1,10 @@
 import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
 import React, { useState } from 'react'
+import { API_URI } from '@/constant/app'
+import { useAuthStore } from '@/store/authStore'
 
 interface  ItemUser {
+  _id: string
   image: string
     user: {
       image: string;
@@ -11,13 +14,50 @@ interface  ItemUser {
   }
 }
 
+interface Book {
+  _id: string;
+  image: string;
+  title: string;
+  description?: string;
+  user: {
+    image: string;
+    username: string;
+    user: string;
+  };
+}
+
 const Home = () => {
   const [loading, setLoading] = useState(true)
   const [refreshing, setRefreshing] = useState(false)
-  const [page, setPage] = useState(1)
+  const [pageNum, setPageNum] = useState(1)
   const [hasMore, setHasMore] = useState(true)
-  const [books, setBooks] = useState([])
+  const [books, setBooks] = useState<Book[]>([])
 
+  const { token } = useAuthStore()
+  const fetchBooks =async() => {
+    try {
+      if(refreshing) setRefreshing(true)
+        else if(pageNum === 1) setLoading(true)
+
+        const res = await fetch(`${API_URI}/book/get-book?page=${pageNum}&limit=5`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+          }
+        })
+        const data = await res.json()
+        if(!res.ok) throw new Error(data.message || "Fail to fetch books")
+
+       /* `setBooks((prev) => [...prev, ...data.books])` is updating the state of the `books` array by
+       adding new books fetched from the API response to the existing books array. */
+        setBooks((prev) => [...prev, ...data.books])
+        setHasMore(pageNum < data.total); // Optional: track if more books are available
+    } catch (error) {
+      
+    }
+
+  }
 
   const renderItem=({ item }:{item: ItemUser}) => (
     <View style={styles.bookCard}>
