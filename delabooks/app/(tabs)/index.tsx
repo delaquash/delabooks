8317c4c00eae9f25ac951a263425c029042c9848
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-26 18:20:20 +0100

    feat: implement infinite scroll for book fetching and enhance loading states in Home component

--- delabooks/app/(tabs)/index.tsx
+++ delabooks/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import { ActivityIndicator, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { API_URI } from '@/constant/app'
 import { useAuthStore } from '@/store/authStore'
 import { Image } from 'expo-image';
@@ -47,12 +47,12 @@ const Home = () => {
   const [rating, setRating] = useState(3)
 
   const { token } = useAuthStore()
-  const fetchBooks =async() => {
+  const fetchBooks =async(pageNum=1, refreshing=false) => {
     try {
       if(refreshing) setRefreshing(true)
         else if(pageNum === 1) setLoading(true)
 
-        const res = await fetch(`${API_URI}/book/get-book?page=${pageNum}&limit=5`, {
+        const res = await fetch(`${API_URI}/book/get-book?page=${pageNum}&limit=2`, {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
@@ -64,17 +64,31 @@ const Home = () => {
 
        /* `setBooks((prev) => [...prev, ...data.books])` is updating the state of the `books` array by
        adding new books fetched from the API response to the existing books array. */
-        setBooks((prev) => [...prev, ...data.books])
+        // setBooks((prev) => [...prev, ...data.books])
+
+        /* The code snippet you provided is creating a new array `uniqueBooks` by combining the existing
+        `books` array with the new `data.books` array fetched from the API response. Here's a breakdown of
+        what the code is doing: */
+        const uniqueBooks = 
+          refreshing || pageNum === 1 
+          ? data.books
+          : Array.from(new Set([...books, ...data.books].map((book)=> book._id))).map((id)=> 
+            [...books, ...data.books].find((book) => book._id === id));
+
+        setBooks(uniqueBooks);
         setHasMore(pageNum < data.totalPages); // Optional: track if more books are available
         setPageNum(pageNum)
     } catch (error) {
-      console.error("Error fetching books", error)
+      console.log("Error fetching books", error)
     } finally {
       if(refreshing) setRefreshing(false)
       else setLoading(false)
     }
-
   }
+
+  useEffect(()=> {
+    fetchBooks()
+  }, [])
 
    const RenderRatePicker =(rating: number) => {
       const starRatings = [];
@@ -116,6 +130,12 @@ const Home = () => {
     </View>
   )
 
+  const handleLoadMore = async() => {
+    if(hasMore && !loading && !refreshing){
+        await fetchBooks(pageNum + 1)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <FlatList 
@@ -124,7 +144,35 @@ const Home = () => {
         keyExtractor={(item)=> item._id}
         contentContainerStyle={styles.contentContainer}
         showsVerticalScrollIndicator={false}
+
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={0.1}
+        ListHeaderComponent={
+          <View style={styles.header}>
+            <Text style={styles.headerTitle}>BookWorm</Text>
+            <Text style={styles.headerSubTitle}>Discover great reads from the community</Text>
+          </View>
+        }
+        ListFooterComponent={
+          hasMore && books.length > 0 ? (
+            <ActivityIndicator  style={styles.footerLoader} color={COLORS.primary} size="small" />
+          ) :(
+            null
+          )
+        }
+        ListEmptyComponent={
+          <View style={styles.emptyContainer}>
+            <Ionicons 
+              name="book-outline"
+              size={60}
+              color={COLORS.textSecondary}
+            />
+            <Text style={styles.emptyText}>No recommendations yet</Text>
+            <Text style={styles.emptySubText}>Be the first to share a book </Text>
+          </View>
+        }
       />
+    
     </View>
   )
 }
@@ -146,5 +194,12 @@ const styles = StyleSheet.create({
   BookTitle:{},
   bookDetails:{},
   caption:{},
-  date:{}
+  date:{},
+  header:{},
+  headerTitle:{},
+  headerSubTitle:{},
+  emptyContainer:{},
+  emptyText:{},
+  emptySubText: {},
+  footerLoader:{}
 })
